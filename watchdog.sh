ae866942de2f1e1356a8d26300b3091a7411a929
Ajil Raju <ajilraju01@gmail.com>
2018-09-26 16:46:34 +0530

    cmd option selector template added

--- watchdog.sh
+++ watchdog.sh
@@ -8,13 +8,42 @@ function usage() {
     echo -e "The command are:\n"
     echo -e "\tunit        Units running on system.\n
         process     Process ID uses for filtering.\n
-        UID         User ID uses for filtering.\n
+        uid         User ID uses for filtering.\n
         path        Path location of the program.\n
         kernel      Display the kernel messages.\n
-        priority    Message priority at verbose level.\n"
+        prty        Message priority at verbose level.\n"
 }
 
+# command option selector to further process.
+function opt_selector() {
+    case "$1" in 
+        "unit")
+            #
+            ;;
+        "process")
+            #
+            ;;
+        "uid")
+            #
+            ;;
+        "path")
+            #
+            ;;
+        "kernel")
+            #
+            ;;
+        "prty")
+            #
+            ;;
+        *)
+            echo "Usage: $0 (unit|process|uid|path|kernel|prty)"
+            ;;
+esac
+        
+}
 # to check wether command with no arguments.
 if [ -z "$1" ]; then
     usage
+else 
+    opt_selector $1
 fi
