50b57e0345718e4d55d04cddbb68d2adbbef84c5
Ajil Raju <ajilraju01@gmail.com>
2018-09-26 14:27:01 +0530

    command usage verbose added

--- watchdog.sh
+++ watchdog.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+function usage() {
+    echo -e "watchdog.sh is a logging tool.\n"
+    echo -e "Usage:\n"
+    echo -e "\twatchdog.sh command [arguments]\n"
+    echo -e "The command are:\n"
+    echo -e "\tunits       Units running on system.\n
+        process     Process ID uses for filtering.\n
+        UID         User ID uses for filtering.\n
+        path        Path location of the program.\n
+        kernel      Display the kernel messages.\n
+        priority    Message priority at verbose level.\n"
+}
+
+usage
