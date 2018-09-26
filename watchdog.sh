515abea45f704ef06f4fd7aec53cedbec463f51f
Ajil Raju <ajilraju01@gmail.com>
2018-09-26 16:25:25 +0530

    completed the usage section

--- watchdog.sh
+++ watchdog.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# command usage option and args display.
 function usage() {
     echo -e "watchdog.sh is a logging tool.\n"
     echo -e "Usage:\n"
     echo -e "\twatchdog.sh command [arguments]\n"
     echo -e "The command are:\n"
-    echo -e "\tunits       Units running on system.\n
+    echo -e "\tunit        Units running on system.\n
         process     Process ID uses for filtering.\n
         UID         User ID uses for filtering.\n
         path        Path location of the program.\n
@@ -13,4 +14,7 @@ function usage() {
         priority    Message priority at verbose level.\n"
 }
 
-usage
+# to check wether command with no arguments.
+if [ -z "$1" ]; then
+    usage
+fi
